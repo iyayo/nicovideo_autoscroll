b0201d25cd6fe8a1f939a3acd9e7b17d1bd663f6
ichijiku001 <toraneko2525ry@gmail.com>
2021-03-17 01:38:10 +0900

    [update] UIの変更&プレビューの追加

--- js/options.js
+++ js/options.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     restore_option();
+    const tag = document.getElementById("tag");
+    const previewImage = document.getElementById("previewImage");
     const save_button = document.getElementById("save");
-    save_button.addEventListener("click", () => {
-        save_option();
-    });
+    
+    save_button.addEventListener("click", save_option);
+
+    tag.addEventListener("change", () => {
+        previewImage.src = `/image/${tag.value.slice(1)}.png`;
+    })
+
+    function save_option() {
+        const status = document.getElementById("status");
+        chrome.storage.local.set({ "nvas_tag": tag.value }, () => {
+            status.className = "show";
+            setTimeout(() => {
+                status.className = "hide";
+            }, 2000);
+        });   
+    }
+    
+    function restore_option() {
+        chrome.storage.local.get(["nvas_tag"], (value) => {
+            if (value.nvas_tag !== undefined) {
+                const option_list = document.querySelectorAll("option");
+                option_list.forEach((option) => {
+                    if (value.nvas_tag === option.value){
+                        option.selected = true;
+                        previewImage.src = `/image/${option.value.slice(1)}.png`;
+                    }
+                });
+            }
+        });
+    }
 });
-
-
-function save_option() {
-    let tag = document.getElementById("tag").value;
-    chrome.storage.local.set({ "nvas_tag": tag }, () => {
-        let status = document.getElementById("status");
-        status.innerText = "保存しました";
-        setTimeout(function () {
-            status.innerText = " ";
-        }, 1500);
-    });   
-}
-
-function restore_option() {
-    chrome.storage.local.get(["nvas_tag"], (value) => {
-        if (value.nvas_tag !== undefined) {
-            const option_list = document.querySelectorAll("option");
-            option_list.forEach((option) => {
-                if (value.nvas_tag === option.value){
-                    option.selected = true;
-                }
-            });
-        }
-    });
-}
