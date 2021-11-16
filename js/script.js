35d3fd76bb6432a5eef42be6c9401c4153c3c3dd
ichijiku <toraneko2525ry@gmail.com>
2021-11-16 19:53:00 +0900

    [update] コードの整形＆不要なコードの削除

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 let tag = ".TagContainer";
 
-chrome.storage.local.get(["nvas_tag"], (result) => {
-    if (result.nvas_tag !== undefined) {
-        tag = result.nvas_tag;
-    }
+const observer = new MutationObserver(() => windowScroll(tag));
+const target = document.querySelector("title");
+const config = { childList: true };
+
+observer.observe(target, config);
+
+chrome.storage.local.get(["nvas_tag"], result => {
+    if (result.nvas_tag !== undefined) tag = result.nvas_tag;
 
     windowScroll(tag);
 });
 
 chrome.storage.local.onChanged.addListener(result => {
-    if (result.nvas_tag){
-        tag = result.nvas_tag.newValue;
-    }
+    if (result.nvas_tag) tag = result.nvas_tag.newValue;
 });
-
-const observer = new MutationObserver(() => {
-    windowScroll(tag);
-});
-
-let target = document.querySelector("title");
-const config = { childList: true, characterData: true };
-observer.observe(target, config);
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message == "click") {
-        windowScroll(tag);
-    }
+    if (request.message == "click") windowScroll(tag);
 });
 
 function windowScroll(tag) {
@@ -41,7 +33,7 @@ function windowScroll(tag) {
 
             window.scrollBy(0, -header.clientHeight);
         };
-        
+
         clearInterval(interval);
     }, 500);
 }
